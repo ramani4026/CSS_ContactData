bd0345d7905c59336b2016cdf69633ce5c033018
Aditya-spark <56146249+Aditya-spark@users.noreply.github.com>
2019-10-14 15:47:53 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -5,7 +5,7 @@ var util = require('util');
 const Path = require('path');
 const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
 var util = require('util');
-var http = require('https');
+var https = require('https');
 
 exports.logExecuteData = [];
 
